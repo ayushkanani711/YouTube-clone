9ffea3cb132abe1860f6ff51e1e34d4ccfdc10db
ayushkanani711 <ayushkanani2003@gmail.com>
2023-09-19 22:11:04 +0530

    fix thumbnail problem

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
-import { Box, colors } from '@mui/material';
+import { Box } from '@mui/material';
 import {Navbar,Feed,VideoDetail,ChannelDetail,SearchFeed } from './components'
 
 function App(){
--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -11,7 +11,7 @@ function VideoCard({video: {id:{videoId},snippet}}) {
       <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
         {/* <CardMedia image={snippet?.thumbnails?.default?.url} alt ={snippet?.title} sx={{width:358,hight:180}} /> */}
   <CardContent sx={{background:'#1e1e1e' , hight:'80px', padding:0 }} >
-    <img src={snippet.thumbnails.default.url || demoThumbnailUrl} height={180} width={{xs:'100%', sm:'358px',md:'320px'}} />
+    <img src={snippet.thumbnails.default.url || demoThumbnailUrl} height={240} width={{xs:'100%', sm:'358px',md:'320px'}} />
     <div style={{maxWidth :'42ch',overflow:'hidden',textoverflow: 'ellipsis' ,color:'white'}}>{snippet.title.slice(0,60) || demoVideoTitle.slice(0,60)}</div>
     <Typography variant="subtitle2" fontWeight="bold" color="grey" >
       {snippet.channelTitle || demoChannelTitle}
